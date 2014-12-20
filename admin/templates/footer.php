c93a0de6b9df23b536d30e984798c78a845f7397
sagenda <sagenda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-20 20:12:44 +0000

    Add the new version 1.0.4 and remove the trunk
    git-svn-id: https://plugins.svn.wordpress.org/sagenda/trunk@1050472 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/templates/footer.php
+++
@@ -1,4 +0,0 @@
-<br style="clear:both;" />
-<p class="help">Add this tag [sagenda-wp] in your pages, posts or in template.</p>
-<p class="help">**NOTE: You first need to register on Sagenda and then you will get a Authentication token which you will use to validate this Sagenda Plugin. **</p>
-<p class="help">To learn more about our booking easy service please visit : <a href="http://www.sagenda.com/">www.sagenda.com</a></p>
